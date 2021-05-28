6fe9ace7b5c6b0dd83b3224822b1dc4392fb6912
Jhon Pereira <ablue968@gmail.com>
2021-05-28 17:29:29 +0200

    services fixed!

--- src/components/Services.js
+++ src/components/Services.js
@@ -10,7 +10,7 @@ function Services() {
         <h1 className="py-5 text-uppercase"> My services</h1>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-3 col-sm-6">
+                    <div className="col-md-3 col-sm-6 mobile-fix">
                         <div className="box p-3 mb-4 rounded-3">
                             <div className="circle rounded-circle m-auto position-relative">
                                 <FontAwesomeIcon className="icon position-absolute top-50 start-50 translate-middle" icon={faDesktop} size="2x" />
@@ -20,7 +20,7 @@ function Services() {
                         </div>
                     </div>
                     {/*----------------------------------- */}
-                    <div className="col-md-3 col-sm-6">
+                    <div className="col-md-3 col-sm-6 mobile-fix">
                         <div className="box p-3 mb-4 rounded-3">
                             <div className="circle rounded-circle m-auto position-relative">
                                 <FontAwesomeIcon className="icon position-absolute top-50 start-50 translate-middle" icon={faFileCode} size="2x" />
@@ -30,7 +30,7 @@ function Services() {
                         </div>
                     </div>
                     {/*----------------------------------- */}
-                    <div className="col-md-3 col-sm-6">
+                    <div className="col-md-3 col-sm-6 mobile-fix">
                         <div className="box p-3 mb-4 rounded-3">
                             <div className="circle rounded-circle m-auto position-relative">
                                 <FontAwesomeIcon className="icon position-absolute top-50 start-50 translate-middle" icon={faFacebookF} size="2x" />
@@ -40,7 +40,7 @@ function Services() {
                         </div>
                     </div>
                     {/*-------------------------------------- */}
-                    <div className="col-md-3 col-sm-6">
+                    <div className="col-md-3 col-sm-6 mobile-fix">
                         <div className="box p-3 mb-4 rounded-3">
                             <div className="circle rounded-circle m-auto position-relative">
                                 <FontAwesomeIcon className="icon position-absolute top-50 start-50 translate-middle" icon={faGoogle} size="2x" />
