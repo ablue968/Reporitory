73f4c3fe85cfb7efbd745e9c893f55878ea35176
Jhon Pereira <ablue968@gmail.com>
2021-05-30 10:54:52 +0200

    experience fixed

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,51 +10,55 @@ const Experience = () => {
             </div>
             <div className="container experience-wrapper overflow-hidden p-5 position-absolute start-50 translate-middle-x ">
                 <div className="timeline-block float-end">
-                    <div className="position-absolute start-50 translate-middle-x position-absolute start-50 translate-middle-x marker float-start me-2"></div>
-                    <div className="timeline-content"></div>
-                    <h3>2014-2015</h3>
-                    <p>
-                        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-                        it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-                        typesetting, remaining essentially unchanged.
-                    </p>
+                    <div className="position-absolute start-50 translate-middle-x marker float-start me-2"></div>
+                    <div className="timeline-content">
+                        <h3>2014-2015</h3>
+                        <p>
+                            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                            it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+                            typesetting, remaining essentially unchanged.
+                        </p>
+                    </div>
                 </div>
                 {/*-------------------------------------------------------------- */}
                 <div className="timeline-block">
                     <div className="position-absolute start-50 translate-middle-x marker float-start me-2"></div>
-                    <div className="timeline-content"></div>
-                    <h3>2015-2016</h3>
-                    <p>
-                        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-                        it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-                        typesetting, remaining essentially unchanged.
-                    </p>
+                    <div className="timeline-content left">
+                        <h3>2015-2016</h3>
+                        <p>
+                            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                            it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+                            typesetting, remaining essentially unchanged.
+                        </p>
+                    </div>
                 </div>
                 {/*-------------------------------------------------------------- */}
                 <div className="timeline-block float-end">
                     <div className="position-absolute start-50 translate-middle-x marker float-start me-2"></div>
-                    <div className="timeline-content"></div>
-                    <h3>2016-2017</h3>
-                    <p>
-                        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-                        it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-                        typesetting, remaining essentially unchanged.
-                    </p>
+                    <div className="timeline-content">
+                        <h3>2016-2017</h3>
+                        <p>
+                            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                            it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+                            typesetting, remaining essentially unchanged.
+                        </p>
+                    </div>
                 </div>
                 {/*-------------------------------------------------------------- */}
                 <div className="timeline-block">
                     <div className="position-absolute start-50 translate-middle-x marker float-start me-2"></div>
-                    <div className="timeline-content"></div>
-                    <h3>2017-2018</h3>
-                    <p>
-                        is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-                        it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-                        typesetting, remaining essentially unchanged.
-                    </p>
+                    <div className="timeline-content left">
+                        <h3>2017-2018</h3>
+                        <p>
+                            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                            it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+                            typesetting, remaining essentially unchanged.
+                        </p>
+                    </div>
                 </div>
                 {/*-------------------------------------------------------------- */}
             </div>
@@ -63,4 +67,3 @@ const Experience = () => {
 }
 
 export default Experience
-
